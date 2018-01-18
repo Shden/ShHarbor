e6ea2aaf8c026576dcdbf054fe9e3891f299bfe6
Den Afanassiev <den@dataart.com>
2018-01-18 18:37:38 +0300

    common code (OTA, DS1820) is extracted to be reusable, then linked to Switch and Thermo projects using symbolic links.
    All ESP code groupped under the same /esp directory.

--- thermostat/src/OTA.h
+++
@@ -1,7 +0,0 @@
-#ifndef OTA_H
-#define OTA_H
-
-void updateFirmware(int currentVersion, const char* repositoryBaseUrl);
-void updateSpiffs(int currentVersion, const char* repositoryBaseUrl);
-
-#endif
